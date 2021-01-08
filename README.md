649e456a54b370af5b35169cecdf5214f194601b
wangfpp <wangfpp@gmail.com>
2021-01-08 17:58:56 +0800

    完成第一个界面

---
+++ README.md
@@ -0,0 +1,5 @@
+### 学习Android
+
+#### 颜色透明度
+[Alpha](https://blog.csdn.net/sinat_33585352/article/details/83744673)
+[调色板](https://colorhunt.co/)
---
+++ app/src/main/java/com/study/learn_android/MainActivity.java
@@ -0,0 +1,139 @@
+package com.study.learn_android;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ProgressBar progressBar;
+    private Button progressButton;
+    private Button goButton;
+    private long clickTime = 0;
+    private Context context;
+    private TextView textViewTime;
+    private final String TAG = "MainActivity";
+    private int timeWhat = 10001;
+    private Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        context = this;
+        initView();
+        // 定时器更新时间
+        handler = new Handler() {
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+                int whatMsg = msg.what;
+                if (whatMsg == timeWhat) {
+                    Bundle bundle = msg.getData();
+                    long time = bundle.getLong("time");
+                    Date date = new Date(time);
+                    SimpleDateFormat formatter = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");
+                    String curr_time = formatter.format(date);
+                    textViewTime.setText(curr_time);
+                    Log.d(TAG, String.valueOf(time));
+                }
+
+                super.handleMessage(msg);
+            }
+        };
+
+        setInterval();
+    }
+
+    /**
+     * 初始化视图
+     */
+    private void initView() {
+        progressBar = findViewById(R.id.progress);
+        progressButton = findViewById(R.id.loding_btn);
+        goButton = findViewById(R.id.search_go_btn);
+        textViewTime = findViewById(R.id.curr_time);
+
+        progressButton.setOnClickListener(this);
+        goButton.setOnClickListener(this);
+    }
+
+    /**
+     * 定时器
+     */
+    private void setInterval() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        Thread.sleep(1000);
+                        long date = new Date().getTime();
+                        Message timeMsg = new Message();
+                        timeMsg.what = timeWhat;
+                        Bundle bundle = new Bundle();
+                        bundle.putLong("time", date);
+                        timeMsg.setData(bundle);
+                        handler.sendMessage(timeMsg);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+    /**
+     * 物理返回按键的事件处理
+     */
+    @Override
+    public void onBackPressed() {
+        long timeMillseconds = new Date().getTime();
+        if (timeMillseconds - clickTime < 300 && clickTime > 0) {
+            super.onBackPressed();
+        } else {
+            Toast toast = Toast.makeText(context, "点击两次退出APP", Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.show();
+            clickTime = timeMillseconds;
+        }
+    }
+
+    /**
+     * 给元素设置点击事件
+     * @param v
+     */
+    @Override
+    public void onClick(View v) {
+        int nodeId = v.getId();
+        switch (nodeId) {
+            case R.id.loding_btn:
+                // 设置loading显示和隐藏
+                int isVisibility = progressBar.getVisibility();
+                if (isVisibility == View.GONE || isVisibility == View.INVISIBLE) {
+                    progressBar.setVisibility(View.VISIBLE);
+                } else {
+                    progressBar.setVisibility(View.GONE);
+                }
+                break;
+            case R.id.search_go_btn:
+                Intent intent = new Intent(this, SwiperAcivity.class);
+                startActivity(intent);
+                Toast.makeText(this, "去哪?", Toast.LENGTH_LONG).show();
+                break;
+        }
+    }
+}
